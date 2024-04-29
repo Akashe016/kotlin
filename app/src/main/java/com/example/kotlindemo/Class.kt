1f53af85436137102e9a05e1882ca8eaf8d49dc5
Siddharthan <siddharthan.anbazhagan@gmail.com>
2024-04-29 13:17:30 +0530

    added

---
+++ app/src/main/java/com/example/kotlindemo/Class.kt
@@ -0,0 +1,34 @@
+package com.example.kotlindemo
+
+class Config
+
+class DbConfig( // primary
+    private var url :String,
+    var port: Int
+) {
+    var dbUrl: String
+        get() = this.url
+        set(value) { this.url = value  }
+
+}
+
+class NwConfig {
+    private var a: Int = 0
+    constructor(a : Int = 0) { // secondary
+        this.a = a
+    }
+    fun connect() = println("Connected")
+    fun disconnect(): Boolean = false
+}
+
+fun main() {
+
+    val cfg: Config = Config()
+
+    val dbCfg = DbConfig(url = "abc", port = 1234)
+    println(dbCfg.dbUrl) // getter
+    dbCfg.dbUrl = "localhost"
+    println(dbCfg.port)
+
+    val nwCfg = NwConfig(67)
+}
